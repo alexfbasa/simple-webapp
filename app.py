2099559d80a0900f724d3a0f1b7bb4a120891b09
alexfbasa <47458685+alexfbasa@users.noreply.github.com>
2023-07-19 10:45:09 +0300

    Update app.py
    d

--- app.py
+++ app.py
@@ -1,34 +1,16 @@
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route("/")
+def main():
+    color = os.environ.get('COLOR', "green") 
+    message = "Welcome !!! This is "+color+" environment" 
+    return message
+
+@app.route('/how are you')
 def hello():
-    return 'Hello!'
+    return 'I am good, how about you?'
 
-@app.route('/howareyou')
-def how_are_you():
-    return 'How are you?'
-
-if __name__ == '__main__':
-    # Use Gunicorn as the WSGI server
-    from gunicorn.app.base import BaseApplication
-    class FlaskApplication(BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            for key, value in self.options.items():
-                self.cfg.set(key, value)
-
-        def load(self):
-            return self.application
-
-    options = {
-        'bind': '0.0.0.0:8080',
-        'workers': 4
-    }
-
-    FlaskApplication(app, options).run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
